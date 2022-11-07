b50065c1271315f3fbedb6f43c427c5571fa606c
Zion <43247450+ziqiangt@users.noreply.github.com>
2022-11-07 15:55:24 +0800

    Update Facade.sv

--- Structural Patterns/Facade.sv
+++ Structural Patterns/Facade.sv
@@ -1,38 +1,116 @@
-// 单例模式，线程安全
-// systemverilog中所有静态变量的初始化是在time 0之前完成
-class Singleton;
-  local static Singleton me;
+// 视频文件
+class VideoFile;
+  protected string name;
+  protected string codecType;
   
-  // 利用protected对new函数做保护，不允许外部直接访问
-  local function new();
-  endfunction : new
+  function new(string name);
+    this.name = name;
+    this.codecType = find_filetype();
+  endfunction
   
-  static function Singleton get();
-    if(me == null) begin
-      me = new();
-      $display("We made a new one!");
+  function string find_filetype();
+    foreach(name[i]) begin
+      if(name[i] == ".") begin
+        return name.substr(i+1, name.len() - 1);
+      end
     end
-    return me;
-  endfunction  
+  endfunction
+  
+  function string getCodecType();
+    return codecType;
+  endfunction
+  
+  function string getName();
+    return name;
+  endfunction
 endclass
 
-module top;
-   Singleton singleton;
+// 编译码器接口
+interface class Codec;
+endclass
 
-   initial begin
-      // 外部不允许通过new()进行创建
-      fork
-         singleton  = Singleton::get();
-         singleton  = Singleton::get();
-         singleton  = Singleton::get();
-         singleton  = Singleton::get();
-         singleton  = Singleton::get();
-         singleton  = Singleton::get();
-         singleton  = Singleton::get();
-         singleton  = Singleton::get();
-      join
-      #1;
-      $finish();
-   end
+// MPEG4压缩格式编译码器
+class MPEG4CompressionCodec implements Codec;
+  string mtype = "mp4";
+endclass
 
+// Ogg压缩格式编译码器
+class OggCompressionCodec implements Codec;
+  string mtype = "ogg";
+endclass
+
+// 编译码器工厂
+class CodecFactory;
+  static function Codec extract(VideoFile file);
+    string mtype = file.getCodecType();
+    if(mtype == "mp4") begin
+      MPEG4CompressionCodec mcodec;
+      mcodec = new();
+      $display("CodecFactory: extracting mpeg audio...");
+      return mcodec;
+    end else begin
+      OggCompressionCodec mcodec;
+      mcodec = new();
+      $display("CodecFactory: extracting ogg audio...");
+      return mcodec;
+    end
+  endfunction
+endclass
+
+// bit读取器
+class BitrateReader;
+  static function VideoFile read(VideoFile file, Codec codec);
+    $display("BitrateReader: reading file...");
+    return file;
+  endfunction
+
+  static function VideoFile convert(VideoFile buffer, Codec codec);
+    $display("BitrateReader: writing file...");
+    return buffer;
+  endfunction
+endclass
+
+// 调音台
+class AudioMixer;
+  function string fix(VideoFile result);
+    $display("AudioMixer: fixing audio...");
+    return "tmp";
+  endfunction
+endclass
+  
+// 外观
+class VideoConversionFacade;
+  function string convertVideo(string fileName, string format);
+    VideoFile file = new(fileName);
+    Codec sourceCodec = CodecFactory::extract(file);
+    Codec destinationCodec;
+    $display("VideoConversionFacade: conversion started.");
+    if (format == "mp4") begin
+      MPEG4CompressionCodec mcodec = new();
+      destinationCodec = mcodec;
+    end else begin
+      OggCompressionCodec mcodec = new();
+      destinationCodec = mcodec;
+    end
+    
+    begin
+      VideoFile buffer;
+      VideoFile intermediateResult;
+      string result;
+      AudioMixer audioMixer = new();
+      buffer = BitrateReader::read(file, sourceCodec);
+      intermediateResult = BitrateReader::convert(buffer, destinationCodec);
+      result = audioMixer.fix(intermediateResult);
+      return result;
+    end
+  endfunction
+endclass
+  
+module test;
+  initial begin
+    string mp4Video;
+    VideoConversionFacade converter = new();
+    mp4Video = converter.convertVideo("youtubevideo.ogg", "mp4");
+    $display("Ouput file name is \"%0s\"", mp4Video);
+  end
 endmodule
